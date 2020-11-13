44ec71927d2ccb214d326e6aa0e590f98e3a318b
franciuine <74317191+franciuine@users.noreply.github.com>
2020-11-13 01:02:21 -0300

    Create Data.java

---
+++ Programação orientada a objetos/Atividade 1/Data.java
@@ -0,0 +1,15 @@
+public class Data{
+	
+	private int dia, mes, ano;
+	
+	public void Converte(int dma){
+		int x = Integer.toString(dma);
+		return x;
+	}
+	
+	//boolean void DataValida(int x){
+	//}
+	
+	//public void Incrementa(int data){
+	//}
+}
